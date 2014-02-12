b7ff1469df401399b021eeb6639f82b0578002f5
docyes <docyes@lilripper.sv.splunk.com>
2014-02-12 09:11:53 -0800

    syntax errors

--- backgeo.js
+++ backgeo.js
@@ -7,7 +7,7 @@ define(['underscore', 'jquery'], function(_, $) {
                 maximumAge: 0
             });
             if (method !== 'read') {
-                options.error(code: 0.1, message: 'Geolocation method ' + method + ' not supported.'});
+                options.error({code: 0.1, message: 'Geolocation method ' + method + ' not supported.'});
                 $deferred.reject();
             }
             if (!!navigator.geolocation) {
